fcebea84f19ef25f73a53ea4227dd68597136fab
melikeyogurtcu <melikeyogurtcu@hotmail.com>
2023-08-28 15:12:10 +0300

    firebase settings

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -42,6 +42,11 @@ class _home_pageState extends State<home_page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back,color: Colors.red,)),
         backgroundColor: Colors.white,
         title: const Text(
           'Popular Movies',
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:movie_app/firebase_options.dart';
 import 'package:movie_app/splash_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
